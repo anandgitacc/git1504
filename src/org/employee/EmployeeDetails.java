9cf437daf311d1f94420dc67641e36e859580ef9
anandgitacc <anand.akmail@gmail.com>
2024-04-15 11:50:03 +0530

    functional

---
+++ src/org/employee/EmployeeDetails.java
@@ -0,0 +1,35 @@
+package org.employee;
+
+
+public class EmployeeDetails { // block or body of the class
+	
+	private void empName() {
+		System.out.println("Employee name : Anand");
+	}
+	
+	private void empId() {
+		System.out.println("Employee id : 1122");
+	}
+	
+	private void empMobile() {
+		System.out.println("Employee mobile : 8939278836");
+	}
+	
+	// main method -> starting point (jvm)
+	public static void main(String[] args) {
+		
+		
+		EmployeeDetails e = new EmployeeDetails();
+		e.empName();
+		e.empId();
+//		e.empMobile();
+		
+		
+	}
+	
+
+	
+	
+	
+	
+}
